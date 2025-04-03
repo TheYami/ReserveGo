f0b331b0d703ef24698e0f798b9168175ee4d4d8
TheYami <luffy90san@gmail.com>
2025-04-03 16:45:55 +0700

    connect to database

--- Backend/main.go
+++ Backend/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"Backend/config"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+
+    // เชื่อมต่อฐานข้อมูล
+    config.ConnectDatabase()
+
+    r.Run(":8080")
 }
